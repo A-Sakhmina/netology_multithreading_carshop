70326bee03aba77fad99345b704d9b5a24ca06e5
Sakhmina <asadullaevasakhmina@gmail.com>
2022-06-10 23:00:18 +0300

    Initial commit for task2

--- src/main/java/car_shop/Seller.java
+++ src/main/java/car_shop/Seller.java
@@ -1,30 +1,46 @@
 package car_shop;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Seller {
     private Shop shop;
+    private ReentrantLock locker = new ReentrantLock(true);
+    private Condition condition = locker.newCondition();
 
     public Seller(Shop shop) {
         this.shop = shop;
     }
 
-    public synchronized void receiveCar() {
-        System.out.println(Thread.currentThread().getName() + " выпустил 1 авто. Итого в салоне "
-                + (shop.getCars().size() + 1) + "шт.");
-        shop.getCars().add(new Car());
-        notifyAll();
+    public void receiveCar() {
+
+        try {
+            locker.lock();
+            System.out.println(Thread.currentThread().getName() + " выпустил 1 авто. Итого в салоне "
+                    + (shop.getCars().size() + 1) + "шт.");
+            shop.getCars().add(new Car());
+            condition.signalAll();
+        } finally {
+
+            locker.unlock();
+        }
     }
 
-    public synchronized Car sellCar() {
+    public Car sellCar() {
+
         try {
+            locker.lock();
             System.out.println(Thread.currentThread().getName() + " зашел в автосалон");
             while (shop.getCars().size() == 0) {
                 System.out.println(Thread.currentThread().getName() + " Машин нет");
-                wait();
+                condition.await();
             }
             System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
             return shop.getCars().remove(0);
         } catch (InterruptedException exception) {
             System.out.println(Thread.currentThread().getName() + " ушёл");
+        } finally {
+            locker.unlock();
         }
         return null;
     }
